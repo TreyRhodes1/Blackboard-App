63e17263a6e252ec0ba4483c2ac2d068ab26023d
TSO5681 <trhodes@shelterinsurance.com>
2019-08-07 12:28:09 -0500

    Refactoring

--- src/main/java/project/school/blackboard/BlackboardConsumerApplication.java
+++ src/main/java/project/school/blackboard/BlackboardConsumerApplication.java
@@ -1,13 +1,13 @@
-package project.school.springreacttemplate;
+package project.school.blackboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringReactTemplateApplication {
+public class BlackboardConsumerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringReactTemplateApplication.class, args);
+		SpringApplication.run(BlackboardConsumerApplication.class, args);
 	}
 
 }
--- src/test/java/project/school/blackboard/BlackboardConsumerApplicationTests.java
+++ src/test/java/project/school/blackboard/BlackboardConsumerApplicationTests.java
@@ -1,4 +1,4 @@
-package project.school.springreacttemplate;
+package project.school.blackboard;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringReactTemplateApplicationTests {
+public class BlackboardConsumerApplicationTests {
 
 	@Test
 	public void contextLoads() {
